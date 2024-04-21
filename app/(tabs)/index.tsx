648a5a196d1a24ecbc60c5582423faeb0d4cb393
Fenton Reid <fentonreid@gmail.com>
2024-04-21 20:40:37 +0100

    Add custom fonts, medium for testing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,10 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { StyleSheet, View, Text } from "react-native";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.title}>Vollkorn Text</Text>
+      <Text style={styles.title2}>Poppins Text</Text>
     </View>
   );
 }
@@ -16,16 +12,22 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontFamily: "Voll",
+    color: "white",
+  },
+  title2: {
+    fontSize: 20,
+    fontFamily: "Poppins",
+    color: "white",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
